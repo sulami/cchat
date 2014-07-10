984204f4d3e428970a42ee8f2d110cd8196443fb
Robin Schroer <sulamiification@gmail.com>
2014-07-11 01:37:40 +0200

    added session authentication

--- cchat.py
+++ cchat.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 
 app.secret_key = 'ABC123'
 
+sessions = {}
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,11 +20,19 @@ def new():
         return redirect(url_for('index'))
     rand = randint(10000, 99999)
     session['id'] = rand
+    sessions[rand] = request.form['password']
     return render_template('chat.html', id=rand)
 
-@app.route('/<int:id>')
+@app.route('/<int:id>', methods=['GET', 'POST'])
 def connect(id):
-    return str(id)
+    if request.method == 'GET':
+        return render_template('passwd.html', id=id)
+    if id not in sessions:
+        return redirect(url_for('index'))
+    if request.form['password'] != sessions[id]:
+        return redirect(url_for('index'))
+    session['id'] = sessions[id]
+    return "success"
 
 if __name__ == '__main__':
     app.run(debug=True)
