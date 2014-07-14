a3d663239ae6d59a1ded8b2a23ad5dce2fbb5ec3
Robin Schroer <sulamiification@gmail.com>
2014-07-14 15:44:42 +0200

    changed structure to provide static messages

--- cchat.py
+++ cchat.py
@@ -11,6 +11,7 @@ app.secret_key = 'ABC123'
 salt = 'abcABC123'
 
 sessions = {}
+content = {}
 
 @app.route('/')
 def index():
@@ -22,17 +23,26 @@ def new():
         return redirect(url_for('index'))
     rand = randint(10000, 99999)
     sessions[rand] = crypt(request.form['password'], salt)
-    return render_template('chat.html', id=rand)
+    return render_template('message.html', id=rand)
+
+@app.route('/new/<int:id>', methods=['GET', 'POST'])
+def write(id):
+    if request.method == 'GET':
+        return redirect(url_for('index'))
+    if id not in sessions:
+        return redirect(url_for('index'))
+    content[id] = request.form['message']
+    return render_template('read.html', id=id, message=content[id])
 
 @app.route('/<int:id>', methods=['GET', 'POST'])
-def connect(id):
+def read(id):
     if request.method == 'GET':
         return render_template('passwd.html', id=id)
     if id not in sessions:
         return redirect(url_for('index'))
     if crypt(request.form['password'], salt) != sessions[id]:
         return redirect(url_for('index'))
-    return "success"
+    return render_template('read.html', id=id, message=content[id])
 
 if __name__ == '__main__':
     app.run(debug=True)
